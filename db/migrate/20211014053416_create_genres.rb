0265ea0d9e382d95167b82dfc6d49e8e20976dbe
“takato15” <gshawks21@gmail.com>
2021-10-14 06:04:21 +0000

    [add]model追加

--- db/migrate/20211014053416_create_genres.rb
+++ db/migrate/20211014053416_create_genres.rb
@@ -1,6 +1,9 @@
 class CreateGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :genres do |t|
+      t.string :name
+      t.datetime :creat_at
+      t.datetime :update_at
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_045831) do
+ActiveRecord::Schema.define(version: 2021_10_14_053416) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 2021_10_14_045831) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
-
   create_table "cart_products", force: :cascade do |t|
     t.integer "product_id"
     t.integer "customer_id"
@@ -32,7 +31,6 @@ ActiveRecord::Schema.define(version: 2021_10_14_045831) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -46,6 +44,13 @@ ActiveRecord::Schema.define(version: 2021_10_14_045831) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "creat_at"
+    t.datetime "update_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "order_details", force: :cascade do |t|
     t.integer "product_id"
@@ -53,6 +58,9 @@ ActiveRecord::Schema.define(version: 2021_10_14_045831) do
     t.integer "quantity"
     t.integer "price"
     t.integer "production_status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string "customer_id"
@@ -63,17 +71,9 @@ ActiveRecord::Schema.define(version: 2021_10_14_045831) do
     t.string "shipping_post_cord"
     t.string "shipping_address"
     t.integer "status"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
-  create_table "shippings", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "name"
-    t.string "post_cord"
-    t.string "address"
 
   create_table "products", force: :cascade do |t|
     t.integer "genre_id"
@@ -82,7 +82,15 @@ ActiveRecord::Schema.define(version: 2021_10_14_045831) do
     t.integer "price"
     t.string "image_id"
     t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
+  create_table "shippings", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "name"
+    t.string "post_cord"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
