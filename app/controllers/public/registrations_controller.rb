bd9151f943aacaabb892cc02aa0a70eaba7f9ecb
utdmn2 <utdmn2@gmail.com>
2021-10-16 11:07:01 +0000

    [Add]会員画面

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -62,6 +62,24 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super(resource)
   # end
   
+  def edit
+    @customer = current_customer
+  end
+  
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+    redirect_to customers_path
+    else
+    render :edit
+    end
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :is_delete, :email)
+  end
+  
   protected
 
   def configure_permitted_parameters
