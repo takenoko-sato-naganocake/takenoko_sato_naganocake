fb7bc5bfb381b67f8d6c4ba368eaa7c56d1440f7
utdmn2 <utdmn2@gmail.com>
2021-10-16 11:10:01 +0000

    [Add]コンフリクト解消

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -61,6 +61,27 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+ 
+  def edit
+    @customer = current_customer
+  end
+  
+  def update
+    @customer = current_customer
+    
+  if
+    @customer.update(customer_params)
+    redirect_to customers_path
+  else
+    render :edit
+  end
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :is_delete, :email)
+  end
   
   def edit
     @customer = current_customer
