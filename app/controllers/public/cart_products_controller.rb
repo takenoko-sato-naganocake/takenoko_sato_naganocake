b5749cfb8eea902d66d2d986344c5bd253ba00a6
chiba97 <ktfkyrya@gmail.com>
2021-10-18 08:28:12 +0000

    cart作成

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -19,7 +19,7 @@ class Public::CartProductsController < ApplicationController
 
   def destroy
     @cart_product = CartProduct.find(params[:id])
-    @cart_product = CartProduct.destroy
+    @cart_product.destroy
     redirect_to cart_products_path
   end
 
