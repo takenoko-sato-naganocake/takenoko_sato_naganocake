1438808bc384046e9c8fdec3124cec349a8ff586
utdmn2 <utdmn2@gmail.com>
2021-10-19 10:00:40 +0000

    [Add]管理者側注文履歴一覧・詳細・製作ステータス、注文ステータスenum

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+    #0は入金待ち（デフォルト）、１は入金確認、２は製作中、３は発送準備中、４は発送済み
+    enum status: { waiting_payment: 0, payment_confirm: 1, in_production: 2, ready_to_ship: 3, shipped: 4 }
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,4 @@
 class OrderDetail < ApplicationRecord
+    #0は着手不可、１は製作待ち、２は製作中、３は製作完了
+    enum production_status: { not_startable: 0, waiting_for_production: 1, in_production: 2, production_complete: 3 }
 end
