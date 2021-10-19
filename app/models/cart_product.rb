7dd904d6c6ef820a024b29a28d160bb463c3a117
chiba97 <ktfkyrya@gmail.com>
2021-10-19 16:07:16 +0000

    注文機能作成

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -2,4 +2,11 @@ class CartProduct < ApplicationRecord
   
   belongs_to :customer
   belongs_to :product
+  
+  def sum_of_price
+    product.taxin_price * quantity
+  end
+  
+  scope :products_of_price, -> { inject(0){ |sum, product| sum + product.sum_of_price } }
+  
 end
