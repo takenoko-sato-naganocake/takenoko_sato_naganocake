9cb32efdfbf7ac8b5d24c5a7db8e799ad060e7f4
“takato15” <gshawks21@gmail.com>
2021-10-20 09:57:16 +0000

    [Add]バリデーション機能追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,12 +8,12 @@ class Customer < ApplicationRecord
   has_many :cart_products, dependent: :destroy
 
   has_many :order, dependent: :destroy
-  
+
 
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: "全角カタカナのみで入力して下さい" }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\u{30fc}]+\z/, message: "全角カタカナのみで入力して下さい" }
   validates :post_cord, presence: true
   validates :address, presence: true
   validates :phone_number, presence: true
